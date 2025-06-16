8afe0a199305fe36ae7e552bfe37ce3f0484571f
xay2001 <1602357810@qq.com>
2025-06-16 17:07:06 +0800

    add taylor pruning scripts

---
+++ scripts/prune/prune_ddpm_taylor_cifar10.sh
@@ -0,0 +1,9 @@
+python ddpm_prune.py \
+--dataset cifar10 \
+--model_path pretrained/ddpm_ema_cifar10 \
+--save_path run/pruned/taylor/ddpm_cifar10_pruned \
+--pruning_ratio $1 \
+--batch_size 128 \
+--pruner taylor \
+--thr 0.05 \
+--device cuda:2 \
