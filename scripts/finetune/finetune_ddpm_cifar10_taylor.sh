a2d422da0bf577b409c7e6f9e9cb311fd8397caa
xay2001 <1602357810@qq.com>
2025-06-17 10:17:46 +0800

    add finetune scripts

--- scripts/finetune/finetune_ddpm_cifar10_taylor.sh
+++ scripts/finetune/finetune_ddpm_cifar10_taylor.sh
@@ -1,9 +1,9 @@
 python ddpm_train.py \
   --dataset="cifar10" \
-  --model_path="run/pruned/random/ddpm_cifar10_pruned" \
-  --pruned_model_ckpt="run/pruned/random/ddpm_cifar10_pruned/pruned/unet_pruned.pth" \
+  --model_path="run/pruned/taylor/ddpm_cifar10_pruned" \
+  --pruned_model_ckpt="run/pruned/taylor/ddpm_cifar10_pruned/pruned/unet_pruned.pth" \
   --resolution=32 \
-  --output_dir="run/finetuned/random/ddpm_cifar10_pruned_post_training" \
+  --output_dir="run/finetuned/taylor/ddpm_cifar10_pruned_post_training" \
   --train_batch_size=128 \
   --num_iters=100000 \
   --gradient_accumulation_steps=1 \
@@ -15,4 +15,4 @@ python ddpm_train.py \
   --ema_max_decay 0.9999 \
   --dropout 0.1 \
   --use_ema \
-  --logger="wandb" \
+  --logger="wandb" 
