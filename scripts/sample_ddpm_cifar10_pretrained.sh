268cd0355857a33923bb767928b87ea4d22495cd
xay2001 <1602357810@qq.com>
2025-06-17 10:30:22 +0800

    modify sample scripts path of output dir

--- scripts/sample_ddpm_cifar10_pretrained.sh
+++ scripts/sample_ddpm_cifar10_pretrained.sh
@@ -1,4 +1,4 @@
 python ddpm_sample.py \
---output_dir run/sample/ddpm_cifar10_pretrained \
+--output_dir run/sample/pretrained/ddpm_cifar10_pretrained \
 --batch_size 128 \
 --model_path pretrained/ddpm_ema_cifar10 \
